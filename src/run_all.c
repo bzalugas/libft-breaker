5da8350f482d3052965f9ba7da0691bcac14c482
bazaluga <bazaluga@student.42.fr>
2024-05-16 18:13:58 +0200

    fix: better explanation of bof cases

--- src/run_all.c
+++ src/run_all.c
@@ -6,7 +6,7 @@
 /*   By: bazaluga <bazaluga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:16:18 by bazaluga          #+#    #+#             */
-/*   Updated: 2024/05/15 13:03:34 by bazaluga         ###   ########.fr       */
+/*   Updated: 2024/05/16 18:13:43 by bazaluga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	run_all(char *particular_fun, void *fun)
 			if (fcts[i].fun_name && !fcts[i].fun)
 				printf("%sMISSING %s.%s\n", RED, fcts[i].fun_name, COLOR_RESET);
 	if (suite->bofCount > 0)
-		printf("%sIf you have failures in %s\"bof cases\"%s it means your function doesn't \
+		printf("%sIf you have %s\"bof cases\"%s it means your function doesn't \
 behave exactly the same as the libc's function or as expected but this is an \"Undefined Behavior\" case.%s\n", WHITE, BOF, WHITE, COLOR_RESET);
 	CuStringDelete(output);
 	CuSuiteDelete(suite);
